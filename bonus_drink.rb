05170f6ad158d32c287cc3dcf0b02a46a878ce55
takus <info@takus.info>
2016-01-18 23:37:02 +0900

    complete bonus-drink

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,16 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    need_drink = 3
+    bonus = 0
+    total = amount
+    while true do
+      bonus += (total - (bonus * need_drink)) / 3
+      break if total == amount + bonus
+      total = amount + bonus
+    end
+    return total
   end
 end
+
+puts BonusDrink.total_count_for(100)
+
